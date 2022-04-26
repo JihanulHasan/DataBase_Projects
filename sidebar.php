3b6de6d826dad83e74266be7f3bd0fe8c74d143d
Mahrina Mohsin Raisa <104423189+ricebowlq@users.noreply.github.com>
2022-04-26 20:10:48 +0600

    added sidebar

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<div class="col-2 sidebar d-flex justify-content-center align-items-center">
+            <div>
+                
+                <a class="nav-link fw-bolder text-dark hover_effect mb-2" href="class-size-distributions.php">CLASS SIZE DISTRIBUTIONS</a>
+                <a class="nav-link fw-bolder text-dark hover_effect mb-2" href="resource-summery.php">RESOURSE SUMMERY</a>
+                <a class="nav-link fw-bolder text-dark hover_effect mb-2" href="resource-utilization.php">RESOURSE UTILIZATION</a>
+                <a class="nav-link fw-bolder text-dark hover_effect mb-2" href="revenue.php">ALL SCHOOL REVENUE</a>
+                <a class="nav-link fw-bolder text-dark hover_effect mb-2" href="classroom-requirements.php">CLASSROOM REQUIREMENTS</a>
+                <a class="nav-link fw-bolder text-dark hover_effect mb-2" href="sets-revenue.php"> SETS REVENUE</a>
+            </div>
+        </div>
